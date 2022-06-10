54945c69d2fdd01dd9d8f1e92138a352c83bd726
Dhakshin Krishna <78342331+Dozerex-creationz@users.noreply.github.com>
2022-06-10 13:36:34 +0530

    updated UI&UX

--- src/components/subComponents/chatBoxComponents/ChatWindow.js
+++ src/components/subComponents/chatBoxComponents/ChatWindow.js
@@ -2,8 +2,33 @@ import React from "react";
 import ChatItem from "./ChatItem";
 import List from "@mui/material/List";
 import { useSelector } from "react-redux";
+import Backdrop from "@mui/material/Backdrop";
+import Paper from "@mui/material/Paper";
 const ChatWindow = (props) => {
   const rooms = useSelector((state) => state.user.credential.rooms);
+  if (rooms.length === 0) {
+    return (
+      <>
+        <Backdrop
+          sx={{
+            position: "inherit",
+            width: "100%",
+            height: "100%",
+            zIndex: 3,
+          }}
+          open={() => {}}
+          onClick={() => {}}
+        >
+          <Paper
+            sx={{ backgroundColor: "#e0ce7e", padding: "2rem" }}
+            elevation={6}
+          >
+            ...try joining "Welcome Friends", by entering the room name in the search bar
+          </Paper>
+        </Backdrop>
+      </>
+    );
+  }
   return (
     <List
       sx={{
